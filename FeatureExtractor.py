c19edc11b75fec90d9c4642837a2860efc76ca7a
momaabna <momaabna@gmail.com>
2018-10-20 11:21:29 +0200

    Normalized with camera Matrix

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
+
 class FeatureExtractor(object):
     GX = 16/2
     GY = 12/2
-    def __init__(self):
+    def __init__(self,K):
         self.orb = cv2.ORB(5000)
         self.last = None
         self.bf = cv2.BFMatcher()
+        self.img =None
+        self.K = K
+        self.Kinv = np.linalg.inv(K)
     def extract(self,img):
         #kps = []
         match =None
+        self.img= img
         sx = img.shape[1]/self.GX
         sy = img.shape[0]/self.GY
         """
@@ -28,10 +35,39 @@ class FeatureExtractor(object):
                 """
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         corners = cv2.goodFeaturesToTrack(gray,3000,0.01,3)
-        
+        ret =[]
         kps = [cv2.KeyPoint(x=f[0][0],y=f[0][1],_size=20) for f in corners]
         kps,dess =self.orb.compute(img,kps)
         if self.last is not None:
-            match =self.bf.match(dess,self.last["des"])
+            match =self.bf.knnMatch(dess,self.last["des"],k=2)
+            for m,n in match:
+                if m.distance<0.75*n.distance:
+                    kp1 = kps[m.queryIdx].pt
+                    kp2 = self.last["kps"][m.trainIdx].pt
+                    ret.append((kp1,kp2))
+            if len(ret)>0:
+                ret =np.array(ret)
+                ret = self.normalize(ret)
+                model, inliers = ransac((ret[:, 0],ret[:, 1]),
+                        FundamentalMatrixTransform,
+                        min_samples=8,
+                        residual_threshold=1,
+                        max_trials=100)
+                
+                ret = ret[inliers]
+                ss,vv,dd =np.linalg.svd(model.params)
+                print dd
+            #match = zip([kps[m.QueryIdx] for m in match],[self.last["kps"][m.trainIdx] for m in match])
         self.last = {"kps":kps,"des":dess}
-        return [kps,dess,match]
+        return ret
+    def add_ones(self,x):
+        return np.concatenate([x,np.ones((x.shape[0],1))],axis=1)
+    def denormalize(self,pt):
+        re = np.dot(self.K,np.array([pt[0],pt[1],1]).T)
+        re /=re[2]
+        return int(round(re[0])),int(round(re[1]))
+        #return int(round(pt[0]+self.img.shape[0]/2)),int(round(pt[1]+self.img.shape[1]/2))
+    def normalize(self,ret):
+        ret[:,0,:] =np.dot(self.Kinv,self.add_ones(ret[:,0,:]).T).T[:,0:2]#self.img.shape[0]/2
+        ret[:,1,:] =np.dot(self.Kinv,self.add_ones(ret[:,1,:]).T).T[:,0:2]#ret[:,:,1]#self.img.shape[1]/2
+        return ret
--- main.py
+++ main.py
@@ -1,61 +1,29 @@
 import cv2
 import numpy as np
 import pygame
+from FeatureExtractor import FeatureExtractor
 W = 1920/2
 H = 1080/2
 cv2.startWindowThread()
 cv2.namedWindow("image")
-# Initiate SIFT detector
-
-
-class FeatureExtractor(object):
-    GX = 16/2
-    GY = 12/2
-    def __init__(self):
-        self.orb = cv2.ORB(5000)
-        self.last = None
-        self.bf = cv2.BFMatcher()
-    def extract(self,img):
-        #kps = []
-        match =None
-        sx = img.shape[1]/self.GX
-        sy = img.shape[0]/self.GY
-        """
-
-        for ry in range(0,img.shape[0],sy):
-            for rx in range(0,img.shape[1],sx):
-                img_chunk =  img[ry:ry+sy,rx:rx+sx]
-                #print img_chunk.shape
-                kp = self.orb.detect(img_chunk,None)
-                #print kp
-                if len(kp)>0:
-                    for p in kp:
-                        p.pt=(p.pt[0]+rx,p.pt[1]+ry)
-                        kps.append(p)
-                dess =self.orb.compute(img,kps)
-                """
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        corners = cv2.goodFeaturesToTrack(gray,3000,0.01,3)
-        
-        kps = [cv2.KeyPoint(x=f[0][0],y=f[0][1],_size=20) for f in corners]
-        kps,dess =self.orb.compute(img,kps)
-        if self.last is not None:
-            match =self.bf.match(dess,self.last["des"])
-        self.last = {"kps":kps,"des":dess}
-        return [kps,dess,match]
-                        
-
-fe = FeatureExtractor()
+F=1
+K = np.array([[F, 0, W/2],[0,F,H/2],[0,0,1]])
+fe = FeatureExtractor(K)
 def imgprocess(img):
     img = cv2.resize(img,(W,H))
-    print img.shape
+    #print img.shape
     
     
-    kp,des,match = fe.extract(img)
-    for p in kp:
-        u,v = map(lambda x:int(round(x)),p.pt)
-        cv2.circle(img,(u,v),color=(0,255,0),radius=3)
-    
+    match = fe.extract(img)
+    if match is None:
+        return
+    print len(match)
+    for p1,p2 in match:
+        u1,v1 = fe.denormalize(p1)
+        u2,v2 = fe.denormalize(p2)
+        cv2.line(img,(u1,v1),(u2,v2),color=(0,255,0))
+    #for m in  match:
+        #print match
     cv2.imshow("image",img)
     cv2.waitKey(1)
 
